fb74877e08f60091542008329fa3bbea84e7afee
Lucas <couto.lucas92@gmail.com>
2018-10-29 18:01:01 -0300

    LoginScreen Layout started

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -3,7 +3,7 @@ import {
     View,
     Text,
     StyleSheet,
-    Image
+    ImageBackground
 } from "react-native";
 
 class LoginScreen extends Component {
@@ -16,8 +16,62 @@ class LoginScreen extends Component {
     //Content
     render() {
         return (
-            <View style={styles.container}>
-                <Text>LoginScreen</Text>
+            <View style={{ flex: 1 }}>
+                <ImageBackground
+                    source={require('../Assets/bg_img.png')}
+                    style={{ flex: 1 }}
+                >
+
+                {/* Company Logo */}
+                <View style = {{ 
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: "center" 
+                }}>
+                    <View style = {{
+                        width: 110,
+                        height: 110,
+                        backgroundColor: 'white',
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}>
+                        <Text style = {{fontWeight: '600'}}>ISENÇÕES BR</Text>
+                    </View>
+                </View>
+
+                {/* Login Area */}
+                <View>
+
+                    <View style = {{
+                        height: 150,
+                        backgroundColor: 'white'
+                    }}>
+
+                    </View>
+
+                    {/* Footer */}
+                    <View style = {{
+                            height: 70,
+                            backgroundColor: 'white',
+                            alignItems: "flex-start",
+                            justifyContent: "center",
+                            borderTopColor: '#e8e8ec',
+                            borderTopWidth: 1,
+                            paddingHorizontal: 25
+                        }}>
+                        <Text style={{
+                            color: '#5a7fdf',
+                            fontWeight: 'bold',
+                            
+                        }}>
+                            Ou entre com sua rede social
+                        </Text>
+
+                        
+                    </View>
+                </View>
+
+                </ImageBackground>
             </View>
         );
     }
